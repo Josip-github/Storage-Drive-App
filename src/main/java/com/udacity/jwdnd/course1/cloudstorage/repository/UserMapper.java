9d487d1e951d5fa915dfc335eab4b64f94c9aa0e
Josip-github <josipcota1@gmail.com>
2021-11-04 11:51:04 +0100

    This commit creates insert() method in UserMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/repository/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/repository/UserMapper.java
@@ -1,7 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.repository;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -9,4 +11,9 @@ public interface UserMapper {
 
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
+
+    @Insert("INSERT INTO USERS(username, salt, password, firstname, lastname) " +
+            "VALUES(#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(User user);
 }
