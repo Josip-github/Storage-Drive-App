d9c74ffa235a697b460f432eb9f3e8a48fdef8e9
Josip-github <josipcota1@gmail.com>
2021-11-04 11:59:13 +0100

    This commit creates UserService class in service package This class contains 3 important methods for checking if a user with the certain username exists in the database, for creating a new User in the database and for retrieving a user from the database.

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,42 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.repository.UserMapper;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+
+    private HashService hashService;
+    private UserMapper userMapper;
+
+    public UserService(HashService hashService, UserMapper userMapper) {
+        this.hashService = hashService;
+        this.userMapper = userMapper;
+    }
+
+    public boolean isUsernameAvailable(String username){
+        return userMapper.getUser(username) == null;
+    }
+
+    public int createUser(User user){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User
+                (null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstname(), user.getLastname()));
+    }
+
+    public User getUser(String username){
+        return userMapper.getUser(username);
+    }
+
+
+
+
+}
