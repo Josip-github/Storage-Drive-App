4ed3e05e69e60d1e674caff0ebfbf73941aa5a0e
Josip-github <josipcota1@gmail.com>
2021-11-04 11:44:13 +0100

    This commit creates UserMapper interface in repository package

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/repository/UserMapper.java
@@ -0,0 +1,12 @@
+package com.udacity.jwdnd.course1.cloudstorage.repository;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM USERS WHERE username = #{username}")
+    User getUser(String username);
+}
