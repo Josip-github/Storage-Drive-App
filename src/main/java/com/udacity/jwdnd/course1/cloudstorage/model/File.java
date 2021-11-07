ea19320b46421ce855a8f6a78f690fcb517cfa42
Josip-github <josipcota1@gmail.com>
2021-11-07 14:33:36 +0100

    This commit creates model class File that matches database table FILES

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,62 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class File {
+
+    private Integer fileId;
+    private String filename;
+    private String contenttype;
+    private String filesize;
+    private Integer userId;
+    private byte[] filedata;
+
+    public File() {
+    }
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getContenttype() {
+        return contenttype;
+    }
+
+    public void setContenttype(String contenttype) {
+        this.contenttype = contenttype;
+    }
+
+    public String getFilesize() {
+        return filesize;
+    }
+
+    public void setFilesize(String filesize) {
+        this.filesize = filesize;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public byte[] getFiledata() {
+        return filedata;
+    }
+
+    public void setFiledata(byte[] filedata) {
+        this.filedata = filedata;
+    }
+}
