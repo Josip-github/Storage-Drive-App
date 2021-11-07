dc2680d49529dc40a08c71db11429697d2a2f1e6
Josip-github <josipcota1@gmail.com>
2021-11-07 14:47:53 +0100

    This commit creates FileMapper

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/repository/FileMapper.java
@@ -0,0 +1,22 @@
+package com.udacity.jwdnd.course1.cloudstorage.repository;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface FileMapper {
+
+    @Select("SELECT * FROM FILES")
+    List<File> getFiles();
+
+    @Select("SELECT * FROM FILES WHERE fileId = #{fileId}")
+    File getFile(Integer fileId);
+
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata) " +
+            "VALUES (#{filename}, #{contenttype}, #{filesize}, #{userId}, #{filedata})")
+    int insert(File file);
+}
