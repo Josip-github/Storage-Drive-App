605da1cad8db3d751db867fbdcee289a205bf1db
Josip-github <josipcota1@gmail.com>
2021-11-07 13:54:00 +0100

    This commit creates uploadFile() method This is just a beginning, later I'll come back to this method in order to finish it, first I need to prepare other stuff for that.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/home")
@@ -11,5 +15,10 @@ public class HomeController {
     @GetMapping
     public String homePageView(){
         return "home";
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model){
+        return "file";
     }
 }
