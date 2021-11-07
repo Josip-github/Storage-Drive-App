2d56cdc3191f7a33ce0653910f59361a122e5b51
Josip-github <josipcota1@gmail.com>
2021-11-07 15:32:44 +0100

    This commit creates fileService field in HomeController The method uploadFile() calls its storeFile() method in order to store a certain file chosen by the user in the database

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    private FileService fileService;
 
     @GetMapping
     public String homePageView(){
@@ -18,7 +24,8 @@ public class HomeController {
     }
 
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model){
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model, Authentication authentication) throws IOException {
+        this.fileService.storeFile(file, authentication);
         return "file";
     }
 }
