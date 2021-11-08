03d47b2e5ad81aac3f5ab0b5f7ef349eac336bfb
Josip-github <josipcota1@gmail.com>
2021-11-08 13:12:15 +0100

    This commit deletes not needed lines of code

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -9,16 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 
 @Controller
-//@RequestMapping("/home")
 public class HomeController {
 
     private FileService fileService;
@@ -69,34 +65,4 @@ public class HomeController {
 
     }
 
-
-
-    /*@PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model, Authentication authentication) throws IOException {
-        this.fileService.storeFile(file, authentication);
-        List<File> fileList = this.fileService.getAllFiles();
-        model.addAttribute("files", fileList);
-        return "home";
-    }
-
-    @PostMapping("/file-upload")
-    public String postFile(@RequestParam("fileUpload") MultipartFile fileUpload, Principal principal, Model model) throws IOException {
-
-        if(fileUpload.isEmpty()){
-            model.addAttribute("success", false);
-            model.addAttribute("message", "No file selected to upload!");
-            return "result";
-        }
-
-        File file = new File();
-        file.setFiledata(fileUpload.getBytes());
-        file.setUserId(this.userService.getUser(principal.getName()).getUserId());
-        file.setFilesize(String.valueOf(fileUpload.getSize()));
-        file.setFilename(fileUpload.getOriginalFilename());
-        file.setContenttype(fileUpload.getContentType());
-        fileService.insert(file);
-        model.addAttribute("success", true);
-        model.addAttribute("message", "New file added successfully!");
-        return "result";
-    }*/
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -1,16 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
-import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.repository.FileMapper;
-import com.udacity.jwdnd.course1.cloudstorage.repository.UserMapper;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @Service
@@ -26,19 +20,6 @@ public class FileService {
     public void postConstruct(){
         System.out.println("Creating FileService bean.");
     }
-
-    /*public int storeFile(MultipartFile file, Authentication authentication) throws IOException {
-        InputStream fis = file.getInputStream();
-        String nameOfFile = file.getOriginalFilename();
-        String typeOfContent = file.getContentType();
-        String sizeOfFile = String.valueOf(file.getSize());
-        String username = authentication.getName();
-        User user = userMapper.getUser(username);
-        Integer idOfUser = user.getUserId();
-        byte[] dataOfFile = fis.readAllBytes();
-        File fileToStore = new File(nameOfFile, typeOfContent,sizeOfFile, idOfUser, dataOfFile);
-        return fileMapper.insert(fileToStore);
-    }*/
 
     public void insert(File file){
         fileMapper.insert(file);
