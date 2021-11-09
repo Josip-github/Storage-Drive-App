0bb77601da9591a2f6bdeaa592336c5a68c6d2f6
Josip-github <josipcota1@gmail.com>
2021-11-09 08:00:41 +0100

    This commit creates NoteService class and among others the method createnewNote() which relies on noteMapper and will be needed in NoteController class

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.repository.NoteMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+
+    private NoteMapper noteMapper;
+
+    public NoteService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    public void createNewNote(Note note){
+        noteMapper.insert(note);
+    }
+}
