1031b379488c91809ada8d5bd3a0fce5b35c0ae8
Josip-github <josipcota1@gmail.com>
2021-11-12 11:39:27 +0100

    With this commit all four crud operations concerning notes are valid The bug lied in the line 33 - I have to set the userId because it's needed for update() method that takes 3 arguments of note object. The two remaining arguments noteTitle and noteDescription are being defined by user in template

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -14,8 +14,8 @@ import java.util.List;
 @Controller
 public class NoteController {
 
-    private final NoteService noteService;
-    private final UserService userService;
+    private NoteService noteService;
+    private UserService userService;
 
     public NoteController(NoteService noteService, UserService userService) {
         this.noteService = noteService;
@@ -30,14 +30,11 @@ public class NoteController {
     @PostMapping("/add-note")
     public String addNewNoteOrUpdate(@ModelAttribute(value = "newNote") Note note, Model model, Authentication authentication){
         User user = userService.getUser(authentication.getPrincipal().toString());
-        model.addAttribute("errorMessage", false);
-        model.addAttribute("successMessage", false);
+        note.setUserId(user.getUserId());
 
         if(note.getNoteId() != null){
             try {
                 noteService.updateNote(note);
-                List<Note> notes = noteService.getAllNotesFromThisUser(user.getUserId());
-                model.addAttribute("notes", notes);
                 model.addAttribute("successMessage", "Your note was edited succesfully.");
                 return "result";
             } catch (Exception e){
@@ -48,12 +45,9 @@ public class NoteController {
             try {
                 Note newNote = new Note(note.getNoteTitle(), note.getNoteDescription(), user.getUserId());
                 noteService.createNewNote(newNote);
-                List<Note> notes = noteService.getAllNotesFromThisUser(user.getUserId());
-                model.addAttribute("notes", notes);
                 model.addAttribute("successMessage", "The note added successfully!");
                 return "result";
             } catch (Exception e){
-                //e.printStackTrace();
                 model.addAttribute("errorMessage", "Something went wrong with adding the note... Please try again!");
                 return "result";
             }
