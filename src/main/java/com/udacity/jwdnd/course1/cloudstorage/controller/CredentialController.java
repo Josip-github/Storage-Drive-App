27d3056167b9c711f600ebb3ed8bc5134e2d95ff
Josip-github <josipcota1@gmail.com>
2021-11-12 17:40:39 +0100

    This commit creates CredentialController class It depends on userService and CredentialService In this commit method for creating new Credential has been made

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CredentialController {
+
+    private UserService userService;
+    private CredentialService credentialService;
+
+    public CredentialController(UserService userService, CredentialService credentialService) {
+        this.userService = userService;
+        this.credentialService = credentialService;
+    }
+
+    @ModelAttribute("credential")
+    public Credential getCredential(){
+        return new Credential();
+    }
+
+    @PostMapping("/add-credential")
+    public String addNewCredential(Credential credential, Authentication authentication, Model model){
+        User user = userService.getUser(authentication.getPrincipal().toString());
+
+        try {
+            credentialService.createNewCredential(credential);
+            model.addAttribute("successMessage", "The credential added successfully!");
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "There was something wrong with adding credential. Please try again.");
+        }
+        return "result";
+    }
+}
